16d4d8f62ddacc9be8d9a2736dc45ea4f946d8df
Felis-Insanis <132434409+Felis-Insanis@users.noreply.github.com>
2024-05-29 14:18:47 +0200

    yea

--- routes.js
+++ routes.js
@@ -1,30 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const mariadb = require('mariadb');
+const pool = mariadb.createConnection({host: 'localhost', user: 'api', password: 'passord_til_APIet'});
+
+router.use(express.json());
 
 router.get("", (req, res) => {
     res.send("UwU");
 });
 
 router.get("/handshake", (req, res) => {
-    const message = req.headers
+    const message = req.headers;
     if (message["pass"] == "OwO") {
-        res.send("UwU")
+        res.send("UwU");
     } else {
-        res.send("nowo")
-    }
+        res.send("nowo");
+    };
 });
 
 router.get("/highscore", (req, res) => {
-    const message = req.headers
+    const message = req.headers;
     if (message["pass"] == "OwO") {
-        res.send("UwU")
+        res.send("UwU");
     } else {
-        res.send("nowo")
-    }
+        res.send("nowo");
+    };
 });
 
 router.post("/add_score", (req, res) => {
-    res.send("UwU");
+    const message = req.headers;
+    const content = req.body;
+    if (message.pass == "OwO") {
+        console.log(req.body.score);
+        res.send("UwU");
+    } else {
+        res.send("nowo");
+    };
+    // res.send(message);
 });
 
 module.exports = router;

b3edda051474f366f7a02941d78a71f761fa48f0
Felis-Insanis <132434409+Felis-Insanis@users.noreply.github.com>
2024-05-30 10:40:14 +0200

    lol

--- routes.js
+++ routes.js
@@ -7,21 +7,22 @@ const pool = mariadb.createPool({
     password: 'passord_til_APIet'
 });
 
-async function getHS() {
-    let conn;
-    try {
-      conn = await pool.getConnection();
-      const rows = await conn.query("SELECT * FROM highscore_db.Scores");
-      console.log(rows); //[ {val: 1}, meta: ... ]
-      const res = await conn.query("INSERT INTO highscore_db.Scores VALUES (?, ?)", ["testing", 1]);
-      console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+// small little tiny cheatsheet
+// async function getHS() {
+//     let conn;
+//     try {
+//       conn = await pool.getConnection();
+//       const rows = await conn.query("SELECT * FROM highscore_db.Scores");
+//       console.log(rows); //[ {val: 1}, meta: ... ]
+//       const res = await conn.query("INSERT INTO highscore_db.Scores VALUES (?, ?)", ["testing", 1]);
+//       console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
   
-    } catch (err) {
-      throw err;
-    } finally {
-      if (conn) return conn.end();
-    }
-  }
+//     } catch (err) {
+//       throw err;
+//     } finally {
+//       if (conn) return conn.end();
+//     }
+// }
 
 router.use(express.json());
 
@@ -38,23 +39,37 @@ router.get("/handshake", (req, res) => { // sjekker kobling
     };
 });
 
-router.get("/highscore", (req, res) => { // får frem highscore
+router.get("/highscore", async (req, res) => { // får frem highscore
+    console.log("Handling HIGHSCORE request...")
     const message = req.headers;
     if (message["pass"] == "OwO") {
         // brukte https://mariadb.com/kb/en/getting-started-with-the-nodejs-connector/ for å løse problemet
-        getHS()
-        res.send("UwU");
+        let conn;
+        conn = await pool.getConnection();
+        const scores = await conn.query("SELECT * FROM highscore_db.Scores");
+        let answer = "nickname             | score\n";
+        for (const name in scores) {
+            answer += `${name}`;                                        //adding name to response
+            for (let index = 0; index < 20 - item.length; index++) {    //adding padding to response
+                answer += " ";
+            }
+            answer += ` | ${scores[name]}\n`;                           //adding score to response
+        }
+        conn.close();
+        res.send(answer);
     } else {
         res.send("nowo");
     };
 });
 
-router.post("/add_score", (req, res) => { // legger til score og nickname til db
+router.post("/add_score", async (req, res) => { // legger til score og nickname til db
+    console.log("Handling ADD SCORE request...")
     const message = req.headers;
     const content = req.body;
     if (message.pass == "OwO") {
         console.log(req.body);
         // pool.then(conn)
+        console.log("Request handled!")
         res.send("UwU");
     } else {
         res.send("nowo");
